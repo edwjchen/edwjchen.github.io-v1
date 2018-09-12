78546d79f4db503a7dab79467493225b0d3613a4
Sleightly <ejc042@ucsd.edu>
2018-09-11 23:55:09 -0700

    smaller image?

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
 	var eTop = window.scrollY;
 	console.log(eTop)
-	if(eTop >= 790){
+	if(eTop >= 820){
     	document.getElementById("profile_name").style.textIndent = "-9999px";
     	document.getElementById("profile_name").style.color = "transparent";
 
@@ -11,7 +11,7 @@ window.onscroll = function() {
 
 	}
 
-	if (eTop > 732) {
+	if (eTop > 740) {
 		document.getElementById("profile_desc_line1").style.textIndent = "-9999px";
 		document.getElementById("profile_desc_line1").style.color = "transparent";
 	} else {
@@ -19,7 +19,7 @@ window.onscroll = function() {
 		document.getElementById("profile_desc_line1").style.color = "white";
 	}
 
-	if (eTop > 712) {
+	if (eTop > 720) {
 		document.getElementById("profile_desc_line2").style.textIndent = "-9999px";
 		document.getElementById("profile_desc_line2").style.color = "transparent";
 	} else {
