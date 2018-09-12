cc3754a5014c0e416b79ce847ae2bf12eefcf367
Sleightly <ejc042@ucsd.edu>
2018-09-12 11:26:38 -0700

    added icon hover

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
 	var eTop = window.scrollY;
 	console.log(eTop)
-	if(eTop >= 820){
+	if(eTop >= 850){
     	document.getElementById("profile_name").style.textIndent = "-9999px";
     	document.getElementById("profile_name").style.color = "transparent";
 
@@ -11,7 +11,7 @@ window.onscroll = function() {
 
 	}
 
-	if (eTop > 740) {
+	if (eTop > 762) {
 		document.getElementById("profile_desc_line1").style.textIndent = "-9999px";
 		document.getElementById("profile_desc_line1").style.color = "transparent";
 	} else {
@@ -19,7 +19,7 @@ window.onscroll = function() {
 		document.getElementById("profile_desc_line1").style.color = "white";
 	}
 
-	if (eTop > 720) {
+	if (eTop > 738) {
 		document.getElementById("profile_desc_line2").style.textIndent = "-9999px";
 		document.getElementById("profile_desc_line2").style.color = "transparent";
 	} else {
