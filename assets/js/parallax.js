897973d46e15f56468241794766f2cf4eb2c494f
Sleightly <ejc042@ucsd.edu>
2018-09-11 23:18:14 -0700

    added contact information

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,14 +1,29 @@
 window.onscroll = function() {
 	var eTop = window.scrollY;
-	if(eTop >= 900){
+	console.log(eTop)
+	if(eTop >= 790){
     	document.getElementById("profile_name").style.textIndent = "-9999px";
+    	document.getElementById("profile_name").style.color = "transparent";
+
 	} else {
 		document.getElementById("profile_name").style.textIndent = "0px";
+		document.getElementById("profile_name").style.color = "white";
+
 	}
 
-	if (eTop > 800) {
-		document.getElementById("profile_desc").style.textIndent = "-9999px";
+	if (eTop > 732) {
+		document.getElementById("profile_desc_line1").style.textIndent = "-9999px";
+		document.getElementById("profile_desc_line1").style.color = "transparent";
 	} else {
-		document.getElementById("profile_desc").style.textIndent = "0px";
+		document.getElementById("profile_desc_line1").style.textIndent = "0px";
+		document.getElementById("profile_desc_line1").style.color = "white";
+	}
+
+	if (eTop > 712) {
+		document.getElementById("profile_desc_line2").style.textIndent = "-9999px";
+		document.getElementById("profile_desc_line2").style.color = "transparent";
+	} else {
+		document.getElementById("profile_desc_line2").style.textIndent = "0px";
+		document.getElementById("profile_desc_line2").style.color = "white";
 	}
 };
