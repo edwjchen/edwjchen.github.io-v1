14a6adb7adffc6ffe0cba14b866e2447682e09c1
Sleightly <ejc042@ucsd.edu>
2018-09-11 19:20:00 -0700

    Updated personal bio

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,10 +1,14 @@
 window.onscroll = function() {
 	var eTop = window.scrollY;
-	console.log(eTop)
-	console.log(document.getElementById("profile_name").style.textIndent)
 	if(eTop >= 900){
     	document.getElementById("profile_name").style.textIndent = "-9999px";
 	} else {
 		document.getElementById("profile_name").style.textIndent = "0px";
+	}
+
+	if (eTop > 800) {
+		document.getElementById("profile_desc").style.textIndent = "-9999px";
+	} else {
+		document.getElementById("profile_desc").style.textIndent = "0px";
 	}
 };
