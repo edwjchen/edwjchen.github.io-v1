293ea3cd803d60d4612f1946974e54d6f8d92cf9
Sleightly <ejc042@ucsd.edu>
2018-09-12 21:06:45 -0700

    added fade animation to text

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,6 +1,10 @@
 window.onscroll = function() {
+	parallaxText();
+	fade();
+};
+
+function parallaxText() {
 	var eTop = window.scrollY;
-	console.log(eTop)
 	if(eTop >= 850){
     	document.getElementById("profile_name").style.textIndent = "-9999px";
     	document.getElementById("profile_name").style.color = "transparent";
@@ -26,4 +30,17 @@ window.onscroll = function() {
 		document.getElementById("profile_desc_line2").style.textIndent = "0px";
 		document.getElementById("profile_desc_line2").style.color = "white";
 	}
-};
+}
+
+function fade() {
+	var elements = document.getElementsByClassName("fade");
+	var i;
+	for (i = 0; i < elements.length; i++) { 
+	 	var rect = elements[i].getBoundingClientRect();
+	 	if ((rect.top) < screen.height - screen.height/4) {
+	 		$(elements[i]).css('visibility','visible').hide().fadeIn("slow");
+	 		elements[i].className = "faded";
+	 	}
+	}
+
+}
