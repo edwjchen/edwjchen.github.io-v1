6463a24bfbfe3a94ce845629f0d88919838215fb
Sleightly <ejc042@ucsd.edu>
2018-09-18 15:21:58 -0700

    added project organizer

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.get('/about', function(req, res){
-  res.sendFile('assets/html/about.html', { root: __dirname });
+app.get('/test', function(req, res){
+  res.sendFile('assets/html/test.html', { root: __dirname });
+})
+
+
+app.get('/lagrange', function(req, res){
+  res.sendFile('assets/html/lagrange.html', { root: __dirname });
 })
 
 var server = http.createServer(app);
