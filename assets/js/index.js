dae3362f1d8e25d0bba0c9189b42fdad7e67a47a
Sleightly <ejc042@ucsd.edu>
2018-10-25 15:17:51 -0700

    minor aethestic changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,9 @@
 var options = {
-	strings: ["> Hello world! My name is Edward Chen ^650 <br><br>> ^200 I am a software engineer ^650 <br><br>> ^200 And a part time Pokemon Go player . . . ^650 <br><br>> ^200 anyways ^100 <br><br>> ^200 Check out some of my personal projects below ^650 <br><br>> ^200 Or contact me at ejc042@ucsd.edu ^650 <br><br>> ^200 Error message: ^650 <br><br>> ^200 Segmentation fault (core dumped) ^650 <br><br>> ^200 .  ^100 <br><br>> ^200 .  ^100 <br><br>> ^200 Null Reference found"],
+	strings: ["> Hello world! My name is Edward Chen ^650 <br><br>> ^200 I am a software engineer ^650 <br><br>> ^200 And a part time Pokemon Go player . . . ^650 <br><br>> ^200 anyways ^100 <br><br>> ^200 Check out some of my personal projects <u>below</u> ^650 <br><br>> ^200 Or contact me at ejc042@ucsd.edu ^650 <br><br>> ^200 Error message: ^650 <br><br>> ^200 Segmentation fault (core dumped) ^650 <br><br>> ^200 .  ^100 <br><br>> ^200 .  ^100 <br><br>> ^200 Null Reference found"],
 	typeSpeed: 20
 }
 
 var typed = new Typed(".text", options)
-
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
