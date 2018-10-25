ae5bb09eec798a1ef38d64aef7e78b06d7980a9b
Sleightly <ejc042@ucsd.edu>
2018-10-25 01:17:35 -0700

    added more messages for now

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 var options = {
-	strings: ["> Hello world! My name is Edward Chen ^1000 <br><br>> ^200 I am a software engineer"],
-	typeSpeed: 40
+	strings: ["> Hello world! My name is Edward Chen ^500 <br><br>> ^200 I am a software engineer ^500 <br><br>> ^200 And a part time Pokemon Go player . . . ^500 <br><br>> ^200 anyways ^100 <br><br>> ^200 Check out some of my personal projects below ^500 <br><br>> ^200 Or contact me at ejc042@ucsd.edu ^500 <br><br>> ^200 Error message: ^500 <br><br>> ^200 Segmentation fault (core dumped) ^500 <br><br>> ^200 .  ^500 <br><br>> ^200 .  ^500 <br><br>> ^200 Resolving error"],
+	typeSpeed: 45
 }
 
 var typed = new Typed(".text", options)
