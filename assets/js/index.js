7c386536646652543c86ca8538070492d2f32496
Sleightly <ejc042@ucsd.edu>
2018-10-25 01:22:07 -0700

    more minor changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 var options = {
-	strings: ["> Hello world! My name is Edward Chen ^500 <br><br>> ^200 I am a software engineer ^500 <br><br>> ^200 And a part time Pokemon Go player . . . ^500 <br><br>> ^200 anyways ^100 <br><br>> ^200 Check out some of my personal projects below ^500 <br><br>> ^200 Or contact me at ejc042@ucsd.edu ^500 <br><br>> ^200 Error message: ^500 <br><br>> ^200 Segmentation fault (core dumped) ^500 <br><br>> ^200 .  ^500 <br><br>> ^200 .  ^500 <br><br>> ^200 Resolving error"],
-	typeSpeed: 45
+	strings: ["> Hello world! My name is Edward Chen ^400 <br><br>> ^200 I am a software engineer ^400 <br><br>> ^200 And a part time Pokemon Go player . . . ^400 <br><br>> ^200 anyways ^100 <br><br>> ^200 Check out some of my personal projects below ^400 <br><br>> ^200 Or contact me at ejc042@ucsd.edu ^400 <br><br>> ^200 Error message: ^400 <br><br>> ^200 Segmentation fault (core dumped) ^400 <br><br>> ^200 .  ^100 <br><br>> ^200 .  ^100 <br><br>> ^200 Null Reference found"],
+	typeSpeed: 44
 }
 
 var typed = new Typed(".text", options)
