b9aed6c5d2c4e3558d502d4c6252dfa2dfa4eb8d
Sleightly <ejc042@ucsd.edu>
2018-10-31 15:30:43 -0700

    updating projects page

--- server.js
+++ server.js
@@ -9,15 +9,6 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.get('/test', function(req, res){
-  res.sendFile('assets/html/test.html', { root: __dirname });
-})
-
-
-app.get('/lagrange', function(req, res){
-  res.sendFile('assets/html/lagrange.html', { root: __dirname });
-})
-
 var server = http.createServer(app);
 
 server.listen(1500);
