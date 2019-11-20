75a46c71cca879866cabfb06174e41af42fe9864
Sleightly <ejc042@ucsd.edu>
2019-11-19 21:49:27 -0800

    added projects section

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -1,7 +1,7 @@
 $(document).on("scroll", function () {
 	var pageTop = $(document).scrollTop()
 	var pageBottom = pageTop + $(window).height()
-	var tags = $(".fade")
+	var tags = $(".fades")
 
 	for (var i = 0; i < tags.length; i++) {
 		var tag = tags[i]
